4bcea8b266b30ac95714773f37b2bb184d48e3d6
hendrimardani <hendrimardani290301@gmail.com>
2023-10-17 17:21:53 -0400

    themes

--- app/src/main/java/com/example/myartist/DetailActivity.kt
+++ app/src/main/java/com/example/myartist/DetailActivity.kt
@@ -1,10 +1,14 @@
 package com.example.myartist
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.example.myartist.databinding.ActivityDetailBinding
+import java.sql.Array
 
 @Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity() {
@@ -21,5 +25,26 @@ class DetailActivity : AppCompatActivity() {
         binding.tvDetailName.text = dataArtists?.name
         binding.tvDetailDescription.text = dataArtists?.desription
         binding.tvDetailPhoto.setImageResource(dataArtists!!.photo)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_share, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Mengirim tombol share
+        when (item.itemId) {
+            R.id.share -> {
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, binding.tvDetailName.text)
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
