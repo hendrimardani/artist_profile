ba24b718c3b0075e46f85cd65cc1a2a7cd399524
hendrimardani <hendrimardani290301@gmail.com>
2023-10-16 23:29:48 -0400

    layout

--- app/src/main/java/com/example/myartist/DetailActivity.kt
+++ app/src/main/java/com/example/myartist/DetailActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myartist
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.myartist.databinding.ActivityDetailBinding
 
+@Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
+
+        val dataArtists = if (Build.VERSION.SDK_INT >= 35) {
+            intent.getParcelableExtra("key_artists", Artists::class.java)
+        } else intent.getParcelableExtra("key_artists")
+
+        binding.tvDetailName.text = dataArtists?.name
+        binding.tvDetailDescription.text = dataArtists?.desription
+        binding.tvDetailPhoto.setImageResource(dataArtists!!.photo)
     }
 }
